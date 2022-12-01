7deb7e0038e0283f31fb1d9b1fd9086895dcd368
Mattia Colbertaldo <mattia.colbertaldo@mail.polimi.it>
2022-12-01 12:02:18 +0100

    update2

--- hw2_1/openmp.cpp
+++ hw2_1/openmp.cpp
@@ -43,6 +43,27 @@ force apply_force(particle_t particle, particle_t neighbor) {
 
 }
 
+// Integrate the ODE
+void move(particle_t& p, double size) {
+    // Slightly simplified Velocity Verlet integration
+    // Conserves energy better than explicit Euler method
+    p.vx += p.ax * dt;
+    p.vy += p.ay * dt;
+    p.x += p.vx * dt;
+    p.y += p.vy * dt;
+
+    // Bounce from walls
+    while (p.x < 0 || p.x > size) {
+        p.x = p.x < 0 ? -p.x : 2 * size - p.x;
+        p.vx = -p.vx;
+    }
+
+    while (p.y < 0 || p.y > size) {
+        p.y = p.y < 0 ? -p.y : 2 * size - p.y;
+        p.vy = -p.vy;
+    }
+}
+
 void init_simulation(particle_t* parts, int num_parts, double size) {
 	// You can use this space to initialize data objects that you may need
 	// This function will be called once before the algorithm begins
@@ -101,5 +122,8 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
 		}
 	}
 
-
+	#pragma omp parallel for schedule(dynamic)
+	for (int i = 0; i < num_parts; ++i) {
+        move(parts[i], size);
+    }
 }
