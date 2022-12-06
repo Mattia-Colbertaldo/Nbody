8ef39d8f10c2cf7605fb1b40865f2c215e340c3b
Mattia Colbertaldo <mattia.colbertaldo@mail.polimi.it>
2022-12-06 17:46:46 +0100

    update

--- hw2_1/mainMPI.cpp
+++ hw2_1/mainMPI.cpp
@@ -32,7 +32,7 @@ void save(std::ofstream& fsave, std::vector<particle_mpi>& parts, int num_parts,
 }
 
 // Particle Initialization
-void init_particles(std::vector<particle_mpi>& parts,std::vector<float>& masses, int num_parts, double size,int part_seed) {
+void init_particles(std::vector<particle_mpi>& parts, std::vector<float>& masses, int num_parts, double size,int part_seed) {
     //int num_parts = parts.size();
     std::random_device rd;
     std::mt19937 gen(part_seed ? part_seed : rd());
@@ -137,9 +137,8 @@ int main(int argc, char** argv) {
 
     std::vector<particle_mpi> parts(num_parts);
     std::vector<float> masses(num_parts);
-    init_particles(parts, masses, num_parts, size, part_seed);
     std::cout << "Trying to init particles..." << std::endl;
-    init_particles(parts, num_parts, size, part_seed);
+    init_particles(parts, masses, num_parts, size, part_seed);
     int rank, mpi_size;
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -163,7 +162,7 @@ int main(int argc, char** argv) {
     
         //for nel tempo: non parallelizzare
         for (int step = 0; step < nsteps; ++step) {
-            simulate_one_step_mpi(parts, num_parts, size);
+            simulate_one_step(parts, num_parts, size);
 
             // Save state if necessary
             if(rank==0)
