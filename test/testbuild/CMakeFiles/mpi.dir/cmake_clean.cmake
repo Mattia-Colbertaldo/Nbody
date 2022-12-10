66deab5d291234bb760eb0de4aa63e1b25766fc7
Mattia Colbertaldo <mattia.colbertaldo@mail.polimi.it>
2022-12-10 17:45:22 +0100

    update directories

---
+++ test/testbuild/CMakeFiles/mpi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpi.dir/main.cpp.o"
+  "CMakeFiles/mpi.dir/main.cpp.o.d"
+  "CMakeFiles/mpi.dir/mpi.cpp.o"
+  "CMakeFiles/mpi.dir/mpi.cpp.o.d"
+  "mpi"
+  "mpi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mpi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
