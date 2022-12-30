37efa2493c713582b7d19d9e098ed1c0d837d5e5
Valentina Moretti <valentina.moretti@mail.polimi.it>
2022-12-30 14:16:01 +0100

    cuda

---
+++ hw2-3/build/CMakeFiles/mpi.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jellyfish/shared-folder/nbody/Nbody/hw2-2/main.cpp" "CMakeFiles/mpi.dir/main.cpp.o" "gcc" "CMakeFiles/mpi.dir/main.cpp.o.d"
+  "/home/jellyfish/shared-folder/nbody/Nbody/hw2-2/particle.cpp" "CMakeFiles/mpi.dir/particle.cpp.o" "gcc" "CMakeFiles/mpi.dir/particle.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
