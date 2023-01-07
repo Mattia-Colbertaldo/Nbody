abb7ffd125ac46453ade5433f22dfa5abaeab951
Mattia Colbertaldo <mattia.colbertaldo@mail.polimi.it>
2023-01-07 01:17:27 +0100

    Urto elastico e anelastico!!! (╯°□°）╯︵ ┻━┻

--- cuda/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/matti/shared-folder/nbody/Nbody/cuda
-# Build directory: C:/Users/matti/shared-folder/nbody/Nbody/cuda/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
